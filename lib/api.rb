50f5f1623aa52f11cba9294e94389b7a02c9257e
Faith Gicheha <fwgicheh@gmail.com>
2020-07-17 11:41:08 -0700

    - response accessor

--- lib/api.rb
+++ lib/api.rb
@@ -1,18 +1,16 @@
 # get data from the internet using scraping or hitting an api
 
 class API 
-  attr_accessor :response
   BASE_URL = "https://restcountries.eu/rest/v2/lang/"
 
- 
   def get_info(selected_language_code)
   #this argument is needed #get requests the server to return either a collection or a single item of collection 
- response = HTTParty.get(BASE_URL + selected_language_code)  
+    response = HTTParty.get(BASE_URL + selected_language_code)  
 #  binding.pry
 
- response.each do |names_hash|
-  Country.new(names_hash["name"])
-
+    response.each do |names_hash|
+      Country.new(names_hash["name"])
+    end 
   end
 end 
 
