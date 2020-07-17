ad1bc4988bfd26b0ffe2c2e90af4b5d204550a50
Faith Gicheha <fwgicheh@gmail.com>
2020-07-17 11:17:35 -0700

    - rm def init response + names_hash

--- lib/api.rb
+++ lib/api.rb
@@ -4,25 +4,15 @@ class API
   attr_accessor :response
   BASE_URL = "https://restcountries.eu/rest/v2/lang/"
 
-  def initialize
-    @response = []
-  end
-
+ 
   def get_info(selected_language_code)
   #this argument is needed #get requests the server to return either a collection or a single item of collection 
- @response = HTTParty.get(BASE_URL + selected_language_code)  
+ response = HTTParty.get(BASE_URL + selected_language_code)  
 #  binding.pry
+
+ response.each do |names_hash|
+  Country.new(names_hash["name"])
+
   end
 end 
 
-# puts countries #this prints out the list of countries when called upon 
-
-
-# print_countries =  response.select {|i| countries.push(i["name"])}
-
-# get_info(selected_language_code)
-
-#  response.select {|i| countries.push(
-#    {name: i["name"], area: i["area"]}
-#    )}
-#selcted_language = "ar"
