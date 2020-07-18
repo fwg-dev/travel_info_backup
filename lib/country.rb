bde161044c8a7ea17a44d670ecbb0df8287878eb
Faith Gicheha <fwgicheh@gmail.com>
2020-07-18 00:14:27 -0700

    +notes to file

--- lib/country.rb
+++ lib/country.rb
@@ -1,12 +1,15 @@
 class Country 
-attr_accessor :name 
-@@all =[]
-def initialize(name)
-  @name = name 
-  @@all << self 
-end 
-def self.all 
-  @@all 
-end 
+  attr_accessor :name 
+  @@all =[]
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
+  def self.clear 
+    @@all.clear
+  end 
 
 end 
