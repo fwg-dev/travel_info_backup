e0b8e7040a0048adf80fd3f491afabcca65e78f3
Faith Gicheha <fwgicheh@gmail.com>
2020-07-18 00:13:21 -0700

    +notes to file

--- lib/api.rb
+++ lib/api.rb
@@ -3,13 +3,13 @@
 class API 
   BASE_URL = "https://restcountries.eu/rest/v2/lang/"
 
-  def get_info(selected_language_code)
-  #this argument is needed #get requests the server to return either a collection or a single item of collection 
-    response = HTTParty.get(BASE_URL + selected_language_code)  
+  def get_info(selected_language_code)#this argument/parameter is needed to populate the country by language (user_input is being called in the cli) 
+ # eg. get_info("ar") = "https://restcountries.eu/rest/v2/lang/ar" : this prints countries that speak arabic 
+    response = HTTParty.get(BASE_URL + selected_language_code) 
 #  binding.pry
-
-    response.each do |names_hash|
-      Country.new(names_hash["name"])
+  
+    response.each do |names_hash|#returns the value one by one 
+      Country.new(names_hash["name"]) #a new instance of Country class = name of country 
     end 
   end
 end 
