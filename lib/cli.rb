91632f0ccbf191d9102b4913c820a891cd050073
Faith Gicheha <fwgicheh@gmail.com>
2020-07-18 00:14:05 -0700

    +notes to file

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,16 +14,18 @@ class CLI
     print "When you select a language from the menu below, the program will display a list of countries that speak that language.".yellow
     space
   while(true)
-    puts "\nWhat language would you like to learn about? Please select a number from the menu and press enter"
+
+    puts "\nWhat language would you like to learn about? Please select a 'number' from the menu and press 'enter'."
       print " \n 1.Arabic \n 2.English\n 3.French \n 4.Spanish\n\n".cyan
     user_input = gets.strip
  
     if user_input == "1"
      @api.get_info("ar")
-     countries_list#for each country print country.name
+     countries_list #using this method to print out the list of countries by user_input 
     elsif user_input == "2"
        @api.get_info("en")
       countries_list
+
     elsif user_input == "3"
       @api.get_info("fr")
     countries_list
@@ -33,7 +35,7 @@ class CLI
     else 
   
     puts "Wrong input, please try again!".red
-    next #equivalent of 'continue' in java? 
+    next 
     end
    
     puts "\nCurious about a different language? press 'y' to continue press any key to exit".magenta
@@ -47,13 +49,15 @@ class CLI
     end  
     end 
   end 
-  def countries_list  
-    Country.all.each do |names_hash|
-    puts "#{names_hash.name}"
+  def countries_list  #creating a new instance of country and grabbing all countries and printing out by name 
+    Country.all.each_with_index do |names_hash, index|
+      puts "#{index}. #{names_hash.name}"
     end 
+    Country.clear
   end 
+ 
 
   def space 
     puts ""
   end 
-  end 
+end 
